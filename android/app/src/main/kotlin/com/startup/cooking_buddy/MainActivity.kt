2523f60e1d3d0f0833a48739858bcfdac398cb46
vishalsingh1910 <vishal444m@gmail.com>
2025-09-13 22:07:22 +0530

    first step firebase integration

--- android/app/src/main/kotlin/com/startup/cooking_buddy/MainActivity.kt
+++ android/app/src/main/kotlin/com/startup/cooking_buddy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cooking_buddy
+package com.startup.cooking_buddy
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
 import 'core/utils/status_bar_utils.dart';
 import 'features/splash/presentation/pages/splash_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Set default status bar style
   StatusBarUtils.setLightStatusBar();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const ProviderScope(child: MyApp()));
 }
